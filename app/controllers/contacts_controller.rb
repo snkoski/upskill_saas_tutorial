ca5b9cdb7b81c651459923705f5b850cef84062c
Shawn Koski <shawnnkoski@gmail.com>
2017-01-04 18:44:11 +0000

    Improver error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,12 +3,15 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
   
+  
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to new_contact_path, notice: "Message sent."
+      flash[:success] = "Message sent."
+      redirect_to new_contact_path
     else
-      redirect_to new_contact_path, notice: "Error."
+      flash[:error] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
     end
   end
   
